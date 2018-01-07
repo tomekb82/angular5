954c5efff0984c4ec6ba62938c29041d85bba4bd
Tomasz Belina <tomasz.belina@igt.com>
2018-01-07 22:38:43 +0100

    static component + attribute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StatusCardComponent } from './status-card/status-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/status-card/status-card.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Attribute } from '@angular/core';
+
+@Component({
+  selector: 'status-card',
+  template: `
+    <div class="card" [ngClass]="{
+      'bg-info text-white': type == 'info',
+      'bg-success text-white': type == 'success',
+      'bg-danger text-white': type == 'error'
+      }">
+      <div class="card-body">
+        {{ bodyText }}
+      </div>
+    </div>
+  \`,
+  `,
+
+  styles: []
+})
+export class StatusCardComponent implements OnInit {
+
+  constructor(
+    @Attribute('text') public bodyText,
+    @Attribute('type') public type) { }
+
+  ngOnInit() {
+  }
+
+}
